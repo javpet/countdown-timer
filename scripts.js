d20ebf25ca6d0d360ed88cb2ccd99b2992280eb1
javpet <javorkai.peter@gmail.com>
2017-10-12 21:15:20 +0200

    added explanation and renamed files

--- scripts.js
+++ scripts.js
@@ -28,7 +28,7 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
 	const minutes = Math.floor(seconds / 60);
 	const remainderSeconds = seconds % 60;
-	const display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`;
+	const display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`; //We want to put a 0 in front fro styling
 	document.title = display;
 	timerDisplay.textContent = display; // We can update the title of the document with actually the time of the display
 	console.log(seconds);
